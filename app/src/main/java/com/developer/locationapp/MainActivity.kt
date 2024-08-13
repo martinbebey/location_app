3510d3ea55d315551cc2649e4a4da5b8a0785555
Martin Bebey <37882687+martinbebey@users.noreply.github.com>
2024-08-13 15:47:40 -0400

    add location permission request launcher

--- app/src/main/java/com/developer/locationapp/MainActivity.kt
+++ app/src/main/java/com/developer/locationapp/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.developer.locationapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import android.Manifest
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.developer.locationapp.ui.theme.LocationAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LocationApp()
                 }
             }
         }
@@ -30,17 +41,58 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LocationApp(){
+    val context = LocalContext.current
+    val locationUtil = LocationUtil(context = context)
+    LocationDisplay(locationUtils = locationUtil, context = context)
+}
+
+@Composable
+fun LocationDisplay(locationUtils: LocationUtil, context: Context) {
+
+    val requestPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestMultiplePermissions(),
+        onResult = { permissions ->
+            if (permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true && permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true) {
+                //permissions granted
+            } else {
+                val rationalRequired = ActivityCompat.shouldShowRequestPermissionRationale(context as MainActivity, Manifest.permission.ACCESS_FINE_LOCATION)
+                        || ActivityCompat.shouldShowRequestPermissionRationale(context, Manifest.permission.ACCESS_FINE_LOCATION)
+
+                if(rationalRequired){
+                    Toast.makeText(context, "Location permission is required for this feature to work", Toast.LENGTH_LONG).show()
+                }
+                else{// if need to manually set in device settings
+                    Toast.makeText(context, "Location permission is required. Please enable it in your device settings", Toast.LENGTH_LONG).show()
+                }
+            }
+        })
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text(text = "Location not available")
+
+        Button(onClick = {
+            if(locationUtils.hasLocationPermissions(context)){
+
+            }
+            else{
+                requestPermissionLauncher.launch(arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ))
+            }
+        }) {
+            Text(text = "Get Location")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LocationAppTheme {
-        Greeting("Android")
-    }
+fun LocationAppPreview(){
+    LocationApp()
 }
